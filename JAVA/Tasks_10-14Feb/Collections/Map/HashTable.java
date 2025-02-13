f5174532457d757c7e8077dae264b94b7fa4d315
ParanthamanCodeSpace <135432702+ParanthamanCodeSpace@users.noreply.github.com>
2025-02-13 19:46:02 +0530

    Update HashTable.java

--- JAVA/Tasks_10-14Feb/Collections/Map/HashTable.java
+++ JAVA/Tasks_10-14Feb/Collections/Map/HashTable.java
@@ -3,27 +3,27 @@ import java.util.*;
 public class HashTable {
     public static void main(String[] args) {
         // Using Hashtable (Can also use HashMap)
-        Hashtable<Integer, String> hmap = new Hashtable<>();
+        Hashtable<Integer, String> htab = new Hashtable<>();
 
         // Adding key-value pairs
-        hmap.put(1, "one");
-        hmap.put(2, "two");
-        hmap.put(3, "three");
+        htab.put(1, "one");
+        htab.put(2, "two");
+        htab.put(3, "three");
 
         // Printing initial map
-        System.out.println("Initial Hashtable: " + hmap);
+        System.out.println("Initial Hashtable: " + htab);
 
         // Updating value for key 2
-        hmap.put(2, "For");
-        System.out.println("Updated Hashtable: " + hmap);
+        htab.put(2, "For");
+        System.out.println("Updated Hashtable: " + htab);
 
         // Removing key 1
-        hmap.remove(1);
-        System.out.println("Hashtable after removal: " + hmap);
+        htab.remove(1);
+        System.out.println("Hashtable after removal: " + htab);
 
         // Traversing the map
         System.out.println("Map Traversal:");
-        for (Map.Entry<Integer, String> e : hmap.entrySet()) {
+        for (Map.Entry<Integer, String> e : htab.entrySet()) {
             System.out.println("Key: " + e.getKey() + " | Value: " + e.getValue());
         }
     }
